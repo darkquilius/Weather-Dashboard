ad4cc47f2be2bbf43c3f2f6a5f9d9dfe40e2ac7a
darkquilius <57682169+darkquilius@users.noreply.github.com>
2020-04-10 17:25:34 -0400

    Add files via upload
    Working Weather Dashboard
    To Do:
    - have previous search buttons show when page is first loaded
    - Have last local storage item be the initial city to load

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ $(document).ready(function() {
     const currentDay = document.getElementById("currentDay");
     currentDay.innerHTML = today
 
-    // Used for ajax calls later
+    // Used for ajax calls later and prepopulating page
     var latitude;
     var longitude;
-    var cityName;
+    var cityName = "atlanta";
+    var items = [];
+    localStorage.setItem("cityNames", '["Atlanta"]')
 
-    //when clicked, search button initiates ajax calls to populate the current weather and future 5 day forecast
-    $("#searchBtn").click(function(e) {
-        e.preventDefault();
-
-        cityName = $("#searchInput").val();
-
-        // First ajax call, populates today's weather and allows object properties to be accessed which are crucial for forecast list population
+    // First ajax call, populates today's weather and allows object properties to be accessed which are crucial for forecast list population
+    function populatePage() {
         $.ajax({
             type: "GET",
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=27d78d8d4af0ca517d71ce5153b2ed8a",
@@ -41,93 +38,86 @@ $(document).ready(function() {
 
                 //dynamically creates and appends today's weather API results
                 var todayWeather = $(`   
-                        <div id="todayWeather" class="card" style="width: 100%; margin-top:10px; margin-bottom: 15px; background: lightblue">
-                        <h5>Today's Weather: ${today}</h5>
-                        <div class="card-body row" >
-                            <div class="col-md-5">
-                                <h3 class="card-text">${cityName}</h3>
-                                <img src=" http://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png">
-                            </div>
-                            <div class="col-md-7">
-                                <p class="card-text" id="temp">Temperature: ${temp}&#176;F</p>
-                                <p class="card-text">Humidity: ${response.current.humidity}%</p>
-                                <p class="card-text">Windspeed: ${response.current.wind_speed} mph</p>
-                                <p class="card-text">UV Index: ${response.current.uvi}</p>
-                            </div>
+                    <div id="todayWeather" class="card" style="width: 100%; margin-top:10px; margin-bottom: 15px; background: lightblue">
+                    <h5>Today's Weather: ${today}</h5>
+                    <div class="card-body row" >
+                        <div class="col-md-5">
+                            <h3 class="card-text">${cityName}</h3>
+                            <img src=" http://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png">
                         </div>
-                    </div>`);
+                        <div class="col-md-7">
+                            <p class="card-text" id="temp">Temperature: ${temp}&#176;F</p>
+                            <p class="card-text">Humidity: ${response.current.humidity}%</p>
+                            <p class="card-text">Windspeed: ${response.current.wind_speed} mph</p>
+                            <p class="card-text">UV Index: ${response.current.uvi}</p>
+                        </div>
+                    </div>
+                </div>`);
                 $("#today-weather").prepend(todayWeather);
 
                 //dynamically creates and appends five day forecast API results
                 for (let i = 0; i < 5; i++) {
                     const element = response.daily[i];
 
+                    // Allows future dates to be pulled from moment
+                    var futureDates = moment().add(i + 1, 'days').format("MMM " + "D " + "YYYY");
+
                     var forecastTemp = Math.round(element.temp.day)
                     var forecastWeather = $(`
-                            <div class="card col" style="margin-left: 18px; background: deepskyblue">
-                            <h5>day slot</h5>
-                            <img src=" http://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png" class="card-img-top" alt="...">
-                            <div card-body >
-                                <p class="card-text">Temp: ${forecastTemp}&#176;F</p>
-                                <p class="card-text">Humid: ${element.humidity}%</p>
-                            </div>
-                        </div>`)
+                        <div class="card col" style="margin-left: 18px; background: deepskyblue">
+                        <h5>Date: <br>
+                        ${futureDates}</h5>
+                        <img src=" http://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png" class="card-img-top" alt="...">
+                        <div card-body >
+                            <p class="card-text">Temp: ${forecastTemp}&#176;F</p>
+                            <p class="card-text">Humid: ${element.humidity}%</p>
+                        </div>
+                    </div>`)
 
                     $("#forecast-weather").append(forecastWeather)
                 }
             })
 
         })
+    }
+    populatePage()
+
+    //when clicked, search button initiates ajax calls to populate the current weather and future 5 day forecast
+    $("#searchBtn").click(function(e) {
+        e.preventDefault();
+
+        cityName = $("#searchInput").val();
+
+        function saveCityName() {
+            items.push(cityName);
+            if (cityName) {
+                // sets local storage items and creats buttons to return previous searches
+                localStorage.setItem("cityNames", JSON.stringify(items));
+                $('#previous-search').append('<div class="section">' +
+                    '<br>' +
+                    '<button id="listItem" style="width: 100%">' + items[items.length - 1] + '</button>' +
+                    '</div>');
+
+                $('#searchInput').val("");
+            }
+            // Alerts user if no search input to stop ajax call errors
+            else {
+                alert("Please make sure you enter something before clicking the button");
+            }
+        }
+        // Runs the save and populate functions when save is clicked
+        saveCityName();
+        populatePage();
 
 
 
-
-
-
-
-
-        // $.ajax({
-        //     type: "GET",
-        //     url: "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + latitude + "&lon=" + longitude + "&appid=27d78d8d4af0ca517d71ce5153b2ed8a",
-        // }).then(function(response) {
-        //     console.log(response)
-        //         // current card head's entry will be the value of the search bar
-
-        //     var temp = Math.round(response.current.temp)
-        //     var todayWeather = $(`   
-        //         <div class="card" style="width: 100%; background: lightblue">
-        //         <h5>Today's Weather:</h5>
-        //         <div class="card-body row">
-        //             <div class="col-md-5">
-        //                 <h3 class="card-text">Atlanta</h3>
-        //                 <img src=" http://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png">
-        //             </div>
-        //             <div class="col-md-7">
-        //                 <p class="card-text" id="temp">Temperature: ${temp}&#176;F</p>
-        //                 <p class="card-text">Humidity: ${response.current.humidity}%</p>
-        //                 <p class="card-text">Windspeed: ${response.current.wind_speed} mph</p>
-        //                 <p class="card-text">UV Index: ${response.current.uvi}</p>
-        //             </div>
-        //         </div>
-        //     </div>`);
-        //     $("#today-weather").prepend(todayWeather);
-
-        //     for (let i = 0; i < 5; i++) {
-        //         const element = response.daily[i];
-
-        //         var forecastTemp = Math.round(element.temp.day)
-        //         var forecastWeather = $(`
-        //         <div class="card col" style="margin-left: 18px; background: deepskyblue">
-        //         <h5>day slot</h5>
-        //         <img src=" http://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png" class="card-img-top" alt="...">
-        //         <div card-body >
-        //             <p class="card-text">Temp: ${forecastTemp}&#176;F</p>
-        //             <p class="card-text">Humid: ${element.humidity}%</p>
-        //         </div>
-        //     </div>`)
-
-        //         $("#forecast-weather").append(forecastWeather)
-        //     }
     });
+
+    // Event listener for previous search buttons
+    var cityBtn = document.querySelector("#previous-search");
+    cityBtn.addEventListener("click", function(event) {
+        cityName = event.target.innerText;
+        populatePage();
+    })
 
 });
